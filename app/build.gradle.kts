94cc9ffed900a0d6944c0ae35daf790a465340cb
dannymeienbrock99-prog <dannymeienbrock99@gmail.com>
2026-08-06 22:30:23 +0200

    build: bump Android app to 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.creatorhub.live"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.4.0"
+        versionCode = 11
+        versionName = "1.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

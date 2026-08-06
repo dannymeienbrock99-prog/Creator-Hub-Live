e16d04b3373cd80691d15b18e57f806d569cd35e
dannymeienbrock99-prog <dannymeienbrock99@gmail.com>
2026-08-06 18:09:12 +0200

    Release stable single-camera build as version 1.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.creatorhub.live"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.4"
+        versionCode = 9
+        versionName = "1.3.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

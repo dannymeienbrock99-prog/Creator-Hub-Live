6444f4423ff6333e98709d2d19866978f249aa18
dannymeienbrock99-prog <dannymeienbrock99@gmail.com>
2026-08-06 21:07:38 +0200

    Release Creator Hub Live 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.creatorhub.live"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.3.5"
+        versionCode = 10
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -37,7 +37,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.10.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.github.pedroSG94.RootEncoder:library:2.7.2")
 
     androidTestImplementation("androidx.test:core-ktx:1.6.1")

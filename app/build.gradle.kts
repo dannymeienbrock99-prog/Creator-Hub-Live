081734ddafe79b389a62fb86004db09da11de665
dannymeienbrock99-prog <dannymeienbrock99@gmail.com>
2026-08-06 16:47:48 +0200

    Add WebSocket support for TikFinity chat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.creatorhub.live"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     buildFeatures { viewBinding = true }
@@ -36,5 +36,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.github.pedroSG94.RootEncoder:library:2.7.2")
 }
